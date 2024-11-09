7999e95c5d7c1bff24ad0ab6d5d099410445c1f7
Mir Inayat Ahmed <rimdaas30@gmail.com>
2024-11-09 19:30:55 +0530

    Resolved db.sqlite3 merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ import CommunityPage from './pages/CommunityPage';
 import OrganizationProfilePage from './pages/OrganizationProfilePage';
 import { AuthProvider } from './contexts/AuthContext';
 import LearnMorePage from './pages/LearnMorePage';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
@@ -45,6 +46,7 @@ const App: React.FC = () => {
                 <Route path="/about" element={<LearnMorePage />} />
               </Routes>
             </main>
+            <Footer />
           </div>
         </Router>
       </ThemeProvider>

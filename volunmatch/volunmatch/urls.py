9b9667950b27b01e10453b97ce364705f76a3f8d
Mir Inayat Ahmed <161032591+Mir-Inayat@users.noreply.github.com>
2024-11-12 01:39:01 +0530

    Update urls.py

--- volunmatch/volunmatch/urls.py
+++ volunmatch/volunmatch/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-    path('', views.index)
+    path('', views.index, name='index')
 ]

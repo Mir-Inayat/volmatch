eb3773fcc73a02dcbbfc082a4e4c59a417a52436
Mir Inayat Ahmed <161032591+Mir-Inayat@users.noreply.github.com>
2024-11-12 18:58:06 +0530

    Update urls.py

--- volunmatch/volunmatch/urls.py
+++ volunmatch/volunmatch/urls.py
@@ -22,6 +22,8 @@ from rest_framework_simplejwt.views import (
     TokenBlacklistView,
 )
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),

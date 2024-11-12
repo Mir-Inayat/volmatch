cffa8964ace6566497c9b345833a820b33b37835
Mir Inayat Ahmed <161032591+Mir-Inayat@users.noreply.github.com>
2024-11-12 14:26:42 +0530

    Update settings.py

--- volunmatch/volunmatch/settings.py
+++ volunmatch/volunmatch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'volunmatch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Using pathlib here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

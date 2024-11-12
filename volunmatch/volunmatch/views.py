5d6487b0fbb71fca989d5748e60937a9706bd79a
Mir Inayat Ahmed <161032591+Mir-Inayat@users.noreply.github.com>
2024-11-12 11:29:57 +0530

    Update views.py

--- volunmatch/volunmatch/views.py
+++ volunmatch/volunmatch/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 import os
+from django.conf import settings
 
 def index(request):
     # Serve the index.html directly from static
-    with open(os.path.join('volunmatch/static', 'index.html')) as f:
+    index_path = os.path.join(settings.BASE_DIR, 'volunmatch/static', 'index.html')
+    with open(index_path) as f:
         return HttpResponse(f.read())

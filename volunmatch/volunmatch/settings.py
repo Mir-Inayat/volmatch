0bcade69e305fe3a2a62da153014178e81cfb85e
root <root@vultr.guest>
2024-11-15 07:42:09 +0000

    settings changes

--- volunmatch/volunmatch/settings.py
+++ volunmatch/volunmatch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,5 +145,5 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOW_HEADERS= list(default_headers) + ['ngrok-skip-browser-warning', 'content-type' ]
 APPEND_SLASH = True
